b5f1828a5b32e2c251143dce4fe31d7ffcc3da06
gabrielklemg <79881469+gabrielklemg@users.noreply.github.com>
2021-04-11 22:39:16 -0300

    Update Referências.java

--- Referências.java
+++ Referências.java
@@ -1,8 +1,8 @@
-imagem backgroud pag2-https://www.ctrlplay.com.br/wp-content/uploads/2019/08/Ada-WomenThatDid.jpg
+https://www.ctrlplay.com.br/wp-content/uploads/2019/08/Ada-WomenThatDid.jpg
 https://darkside.blog.br/ada-lovelace-desafiou-o-machismo-e-se-tornou-a-primeira-programadora-da-historia/
 https://proec.ufabc.edu.br/gec/profissao-cientista/ada-lovelace/#:~:text=Ada%20Lovelace%2C%20chamada%20por%20Babbage,de%20f%C3%A1rmacos%20na%20mente%20humana.
 
-imagem backgroud pag6-https://www.ctrlplay.com.br/wp-content/uploads/2019/08/Ada-WomenThatDid.jpg
+https://www.ctrlplay.com.br/wp-content/uploads/2019/08/Ada-WomenThatDid.jpg
 https://wesinalves.github.io/biography/2019/03/08/ada-lovelace.html
 https://blog.locaweb.com.br/artigos/6-curiosidades-sobre-ada-lovelace-que-voce-precisa-saber/
 https://www.imdb.com/title/tt0118882/
